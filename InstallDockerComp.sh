e75b819d543c3804edea522793a5a3c773d3c173
Srimany123 <58987560+Srimany123@users.noreply.github.com>
2022-12-24 10:37:36 +0530

    Update InstallDockerComp.sh

--- InstallDockerComp.sh
+++ InstallDockerComp.sh
@@ -1,5 +1,5 @@
 clear
-read -p "\n\nenter your system architecture : " $arc
+read -p "\n\nenter your system architecture 32 or 64 : " $arc
 if [ $arc -eq 64 ]; then 
 sudo apt install docker-compose64
 else
